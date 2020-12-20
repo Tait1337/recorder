f8839f93a40c31750dbbd0f36f02d6a5347ebd7f
tait1337 <tait1337@gmail.com>
2020-12-20 17:18:14 +0100

    refactor log config file

--- src/main/java/de/clique/westwood/recorder/RecorderApplication.java
+++ src/main/java/de/clique/westwood/recorder/RecorderApplication.java
@@ -3,6 +3,7 @@ package de.clique.westwood.recorder;
 import de.clique.westwood.recorder.ui.ScreenRecorderUI;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
@@ -11,6 +12,7 @@ import java.util.logging.Logger;
  */
 public class RecorderApplication {
 
+    private static final String LOG_CONFIG_FILE = "logging.properties";
     private static final Logger LOGGER = Logger.getLogger(Recorder.class.getName());
 
     public static void main(String[] args) {
@@ -20,9 +22,10 @@ public class RecorderApplication {
 
     private static void loadLoggingSettings() {
         try {
-            LogManager.getLogManager().readConfiguration(RecorderApplication.class.getClassLoader().getResourceAsStream("logging.properties"));
+            InputStream logConfig = RecorderApplication.class.getClassLoader().getResourceAsStream(LOG_CONFIG_FILE);
+            LogManager.getLogManager().readConfiguration(logConfig);
         } catch (IOException e) {
-            LOGGER.severe("Can't read logging.properties file: " + e.toString());
+            LOGGER.severe("Can't read file: " + LOG_CONFIG_FILE + ": " + e.toString());
         }
     }
 
